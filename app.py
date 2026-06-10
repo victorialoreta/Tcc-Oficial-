299697fafefde239e68d914dd4fe2d5bbff431a1
Viquitoria <Victoria@gmail>
2026-06-10 17:25:33 -0300

    10 do 6

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def index():
     return render_template('login.html')
 
 @app.route('/estoque.html')
-def cadastro():
+def estoque():
     return render_template('estoque.html')
+
+@app.route('/editar.html')
+def editar():
+    return render_template('editar.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
