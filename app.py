93d67158dab257483c09be1c7e37e7253d20662a
vic <vic@gmail.com>
2026-07-02 17:18:39 -0300

    02-07

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 
 app = Flask(__name__)
@@ -54,5 +54,69 @@ def editar():
 def novoitem():
     return render_template('novoitem.html')
 
+    nome = request.form.get('nome')
+
+    conexao = mysql.connector.connect(
+        host='localhost',
+        password='',
+        user='root',
+        port=3306,
+        database='almoxarifado'
+    )
+
+    cursor = conexao.cursor()
+    query = "INSERT INTO estoque SET qtde = %s, id = %s, descricao = %s WHERE nome = %s;"
+    valores = (nome,)
+    cursor.execute(query, valores)
+    conexao.commit()
+
+    return redirect("/estoque.html")
+
+@app.route('/salvar_edicao', methods=['POST'])
+def salvar_edicao():
+    
+    id = request.form.get('id')
+    nome = request.form.get('nome')
+    descricao = request.form.get('descricao')
+    qtde = request.form.get('qtde')
+
+    conexao = mysql.connector.connect(
+        host='localhost',
+        password='',
+        user='root',
+        port=3306,
+        database='almoxarifado'
+    )
+
+    cursor = conexao.cursor()
+    query = "UPDATE estoque SET qtde = %s, id = %s, descricao = %s WHERE nome = %s;"
+    valores = (qtde, id, descricao, nome)
+    cursor.execute(query, valores)
+    conexao.commit()
+
+    return redirect("/editar.html")
+
+@app.route('/deletar', methods=['POST'])
+def deletar():
+    
+    nome = request.form.get('nome')
+
+    conexao = mysql.connector.connect(
+        host='localhost',
+        password='',
+        user='root',
+        port=3306,
+        database='almoxarifado'
+    )
+
+    cursor = conexao.cursor()
+    query = "DELETE FROM estoque WHERE nome = %s;"
+    valores = (nome,)
+    cursor.execute(query, valores)
+    conexao.commit()
+
+    return redirect("/editar.html")
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -15,21 +15,28 @@ CREATE TABLE estoque(
 
 SELECT * FROM estoque;
 
-INSERT INTO estoque (nome, qtde, descricao, foto, categoria)
-VALUES ('Parafuso', 500, 'serve pra fixar', 'https://images.cws.digital/produtos/gg/22/20/parafuso-sextavado-zincado-316-x-50-10072022-1670520383817.jpg', 'Miscelaneas');
-
-
-INSERT INTO estoque (nome, qtde, descricao, foto, categoria)
-VALUES ('Alicate', 20, 'serve pra alicatear', 'https://santil.jetassets.com.br/produto/1624594Alicate-Universal-1050-109266474551.jpg', 'ferramenta');
-
+UPDATE estoque
+SET qtde = 400
+WHERE nome = 'Parafuso';
 
 INSERT INTO estoque (nome, qtde, descricao, foto, categoria)
-VALUES ('Chave de fenda', 15, 'serve pra alicatear', 'https://images.cws.digital/produtos/gg/60/58/chave-de-fenda-65x100mm-redstripe-10855860-1759337541133.png', 'ferramenta');
+VALUES ('Parafuso', 500, 'Utilizado para fixação e montagem de peças.', 'https://images.cws.digital/produtos/gg/22/20/parafuso-sextavado-zincado-316-x-50-10072022-1670520383817.jpg', 'Miscelaneas');
 
 
 INSERT INTO estoque (nome, qtde, descricao, foto, categoria)
-VALUES ('Fita crepe', 5, 'serve pra alicatear', 'https://images.tcdn.com.br/img/img_prod/289123/fita_crepe_larga_uso_geral_e_pintura_48mm_x_50m_adere_213773_1_4e4f672dca694e9f35fb8a53bf279b32.jpg', 'Pintura');
+VALUES ('Alicate', 20, 'Ferramenta usada para segurar, cortar ou dobrar materiais.', 'https://santil.jetassets.com.br/produto/1624594Alicate-Universal-1050-109266474551.jpg', 'ferramenta');
 
 
 INSERT INTO estoque (nome, qtde, descricao, foto, categoria)
-VALUES ('Teclado', 6, 'serve pra alicatear', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwu8YTBxX81tsErPtz5f9peP7kfqwYC3z-AHe0EBQA-oCILVecLyftCfM&s=10', 'Informática');
+VALUES ('Chave de fenda', 15, 'Ferramenta usada para apertar e soltar parafusos.', 'https://images.cws.digital/produtos/gg/60/58/chave-de-fenda-65x100mm-redstripe-10855860-1759337541133.png', 'ferramenta');
+
+
+INSERT INTO estoque (nome, qtde, descricao, foto, categoria)
+VALUES ('Fita crepe', 5, 'Fita adesiva utilizada para proteção e marcação.', 'https://images.tcdn.com.br/img/img_prod/289123/fita_crepe_larga_uso_geral_e_pintura_48mm_x_50m_adere_213773_1_4e4f672dca694e9f35fb8a53bf279b32.jpg', 'Pintura');
+
+
+INSERT INTO estoque (nome, qtde, descricao, foto, categoria)
+VALUES ('Teclado', 6, 'Dispositivo de entrada para digitação em computadores.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwu8YTBxX81tsErPtz5f9peP7kfqwYC3z-AHe0EBQA-oCILVecLyftCfM&s=10', 'Informática');
+
+DELETE FROM estoque
+WHERE id = 5;
